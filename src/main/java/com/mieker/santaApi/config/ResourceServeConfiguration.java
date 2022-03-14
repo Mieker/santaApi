e867df573fd3086787577cedf4309a92a83e541f
Mieker <mieker@wp.pl>
2022-03-14 19:21:41 +0100

    WebSecurityConfiguration ready

---
+++ src/main/java/com/mieker/santaApi/config/ResourceServeConfiguration.java
@@ -0,0 +1,22 @@
+//package com.mieker.santaApi.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//
+//@Configuration
+//@EnableResourceServer
+//@EnableGlobalMethodSecurity(jsr250Enabled = true)
+//public class ResourceServeConfiguration extends ResourceServerConfigurerAdapter {
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//
+//        http.authorizeRequests()
+//                .antMatchers("/users/registration").permitAll()
+//                .antMatchers("/users/*/activate").permitAll()
+//                .anyRequest().authenticated();
+//    }
+//}
